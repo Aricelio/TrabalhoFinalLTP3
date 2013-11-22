e5f56ad69c45f7d3fbd49a67a27d12456ddc707f
Aricelio <ariceliosouza@hotmail.com>
2013-11-21 23:27:27 -0200

    SetCategoria foi adicionado a classe Cliente.

--- src/DominModel/Cliente.java
+++ src/DominModel/Cliente.java
@@ -1,24 +1,12 @@
 
 package DominModel;
 
-public class Cliente extends Pessoa{
+public class Cliente extends Pessoa{   
     
-    private Pessoa pessoa;
     
     //Construtor
     public Cliente() {
-    }
-    
-    //Getters e Setters
-    public Pessoa getPessoa() {
-        return pessoa;
-    }
-
-    public void setPessoa(Pessoa pessoa) {
-        this.pessoa = pessoa;
-    }
-    
-    
-    
+        setCategoriaPessoa(CategoriaPessoa.Cliente);
+    }    
     
 }
