73a94fd75ef55b8d344eea1ef629f575ad864512
Aricelio <ariceliosouza@hotmail.com>
2013-11-21 23:35:27 -0200

    Testes feitos na classe Main.java

--- src/DominModel/Main.java
+++ src/DominModel/Main.java
@@ -1,5 +1,7 @@
 
 package DominModel;
+import DataAccess.*;
+import java.util.Date;
 
 public class Main {
 
@@ -7,20 +9,39 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
+        Pessoa p = new Pessoa();
+        PessoaDAO pdao = new PessoaDAO();
+        
+        
+        
+        //Funcionario
         Funcionario f = new Funcionario();
+        FuncionarioDAO fd = new FuncionarioDAO();        
+        Cargo c = new Cargo();
+        
+        f.setCodigo(2);
+        fd.RemoverFuncionario(f);
+        
+        
         Endereco e = new Endereco();
         Email em = new Email();
         
-        //f.setNome("Ari");
-        //e.setComplemento("A");
-        em.setEmail("aricelio@hot");
+        //Cargo
+        c.setCargo("Atendente");
+        c.setCodigo(1);
         
+        //Cliente
+        /*Cliente cli = new Cliente();
+        ClienteDAO clid = new ClienteDAO();
         
+        cli.setNome("Paulo Vitor");
+        cli.setCPF("22222222222");
+        cli.setDataNascimento(new Date());
+        cli.setCategoriaPessoa(CategoriaPessoa.Cliente);
+        cli.setTipoPessoa("Fisica");
+        cli.setRG("17911777");
         
-        
-        //System.out.print(f.getNome());
-        
-        
+        clid.SalvarCliente(cli);*/
         
         
     }
