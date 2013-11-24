6b71d3a46b87db2b1fec9b8c7f4075d5b5e863d1
Aricelio <ariceliosouza@hotmail.com>
2013-11-24 18:51:37 -0200

    Método ListarFuncionarios foi adicionado

--- src/DataAccess/FuncionarioDAO.java
+++ src/DataAccess/FuncionarioDAO.java
@@ -3,11 +3,16 @@ package DataAccess;
 import DominModel.Cargo;
 import DominModel.Funcionario;
 import DataAccess.CargoDAO;
+import DominModel.CategoriaPessoa;
+import DataAccess.UsuarioDAO;
 
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FuncionarioDAO extends PessoaDAO {
 
@@ -53,8 +58,11 @@ public class FuncionarioDAO extends PessoaDAO {
     public boolean RemoverFuncionario(Funcionario fun) {
         if (fun.getCodigo() >= 0) {
             try {
-           
+                UsuarioDAO user = new UsuarioDAO();
+                
+                user.RemoverUsuario(fun.getCodigo());
                 super.RemoverPessoa(fun);
+                
                 return true;
             } catch (Exception ex) {
                 System.err.println(ex.getMessage());
@@ -69,25 +77,22 @@ public class FuncionarioDAO extends PessoaDAO {
             Funcionario funcionario = new Funcionario();
             //ERRO: funcionario não esta recebendo o objeto pessoa
             funcionario = (Funcionario) super.AbrirPessoa(id);
-            
+
             CargoDAO cargoDAO = new CargoDAO();
-            
+
 
             //Seleciona o funcionario e armazena em 'resultado'
-            PreparedStatement sql = getConexao().prepareStatement
-                    ("select * from Funcionarios where codFuncionario=?");
+            PreparedStatement sql = getConexao().prepareStatement("select * from Funcionarios where codFuncionario=?");
             sql.setInt(1, id);
             ResultSet resultado = sql.executeQuery();
-            
-            
 
             //Seleciona o codigo do cargo do funcionario e armazena em 'resultadoCargo'
-            PreparedStatement sqlConsultaCargo = getConexao().prepareStatement
-                    ("select codCargo from Funcionarios where codFuncionario=?");
+            PreparedStatement sqlConsultaCargo = getConexao().prepareStatement("select codCargo from Funcionarios where codFuncionario=?");
             sqlConsultaCargo.setInt(1, id);
             ResultSet resultadoCargo = sqlConsultaCargo.executeQuery();
-            
-            
+
+
+
             if (resultado.next()) {
                 funcionario.setCargo(cargoDAO.AbrirCargo(resultadoCargo.getInt("codCargo")));
                 return funcionario;
@@ -99,6 +104,38 @@ public class FuncionarioDAO extends PessoaDAO {
         } catch (Exception ex) {
             System.err.println(ex.getMessage());
             return null;
+        }
+    }
+
+    //Método ListarFuncionarios
+    public List<Funcionario> ListarFuncionarios() {
+        try {
+            CargoDAO cargoDAO = new CargoDAO();
+
+            PreparedStatement sqlListar = getConexao().prepareStatement
+                    ("select * from Pessoas P join Funcionarios F on P.codPessoa = F.codFuncionario where P.ativo = 1");
+            ResultSet resultado = sqlListar.executeQuery();
+
+            List<Funcionario> listaF = new ArrayList<Funcionario>();
+
+            while (resultado.next()) {
+                Funcionario f = new Funcionario();
+
+                //Carrega os dados da tabela pessoa
+                super.CarregaObjetoPessoa(f, resultado);
+
+                //Carrega o restante dos dados (tabela funcionario)
+                f.setCargo(cargoDAO.AbrirCargo(resultado.getInt("codCargo")));
+                
+
+                listaF.add(f);
+            }
+            
+            return listaF;
+
+        } catch (Exception ex) {
+             System.err.println(ex.getMessage());
+             return null;
         }
     }
 }
