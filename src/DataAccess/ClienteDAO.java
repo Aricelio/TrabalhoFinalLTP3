a889510801d4430280f85dc2d4b74e60b93b09d4
Aricelio <ariceliosouza@hotmail.com>
2013-11-24 05:35:02 -0200

    Método Salvar funcionando.

--- src/DataAccess/ClienteDAO.java
+++ src/DataAccess/ClienteDAO.java
@@ -2,6 +2,10 @@ package DataAccess;
 
 import DominModel.Cliente;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ClienteDAO extends PessoaDAO {
 
@@ -34,4 +38,20 @@ public class ClienteDAO extends PessoaDAO {
         }
         return false;
     }
+    
+    //Método Remover Cliente
+    public boolean RemoverFuncionario(Cliente cli) {
+        if (cli.getCodigo() >= 0) {
+            try {
+           
+                super.RemoverPessoa(cli);
+                return true;
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
+                return false;
+            }
+        }
+        return false;
+    }
+    
 }
