cff1170d3b88eed4e802b1791b70ce3583b9c97b
Aricelio <ariceliosouza@hotmail.com>
2013-12-02 03:52:39 -0200

    Métodos corrigidos.

--- src/DataAccess/UsuarioDAO.java
+++ src/DataAccess/UsuarioDAO.java
@@ -4,9 +4,6 @@ import DominModel.Usuario;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UsuarioDAO extends DAO {
 
@@ -18,14 +15,16 @@ public class UsuarioDAO extends DAO {
     public boolean Salvar(Usuario obj) {
         if (obj.getCodigo() == 0) {
             try {
-                PreparedStatement sql = getConexao().prepareStatement("insert into usuarios(login,senha,codFuncionario) values(?,?,?)");
+                PreparedStatement sql = getConexao().prepareStatement
+                        ("insert into usuarios(login,senha,codFuncionario) values(?,?,?)");
                 sql.setString(1, obj.getLogin());
                 sql.setString(2, obj.getSenha());
                 sql.setInt(3, obj.getFuncionario().getCodigo());
                 sql.executeUpdate();
 
                 //Pega a chave primária que foi gerada no banco de dados
-                PreparedStatement sqlConsulta = getConexao().prepareStatement("select codUsuario from Usuarios where login = ? and senha = ? and codFuncionario = ?");
+                PreparedStatement sqlConsulta = getConexao().prepareStatement
+                        ("select codUsuario from Usuarios where login = ? and senha = ? and codFuncionario = ?");
                 sqlConsulta.setString(1, obj.getLogin());
                 sqlConsulta.setString(2, obj.getSenha());
                 sqlConsulta.setInt(3, obj.getFuncionario().getCodigo());
@@ -43,7 +42,8 @@ public class UsuarioDAO extends DAO {
             try {
                 //Atualiza os dados
                 Connection con = getConexao();
-                PreparedStatement sqlUpdate = con.prepareStatement("update Usuarios set login=?, senha=? where codUsuario=? and codFuncionario=?");
+                PreparedStatement sqlUpdate = con.prepareStatement
+                        ("update Usuarios set login=?, senha=? where codUsuario=? and codFuncionario=?");
                 sqlUpdate.setString(1, obj.getLogin());
                 sqlUpdate.setString(2, obj.getSenha());
                 sqlUpdate.setInt(3, obj.getCodigo());
@@ -62,7 +62,8 @@ public class UsuarioDAO extends DAO {
     //Método Remover Usuario
     public boolean RemoverUsuario(int codFuncionario) {
         try {
-            PreparedStatement sqlRemover = getConexao().prepareStatement("delete from Usuarios where codFuncionario = ?");
+            PreparedStatement sqlRemover = getConexao().prepareStatement
+                    ("delete from Usuarios where codFuncionario = ?");
             sqlRemover.setInt(1, codFuncionario);
             sqlRemover.executeUpdate();
             return true;
